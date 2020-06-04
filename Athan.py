6c9f67c0711cc218a8965b6450d08130f33370e3
haiali <haiali@student.chalmers.se>
2020-06-04 21:29:41 +0200

    Update the Athan.py
    The previous functionality included in the file is more structured and divided in functions

--- Athan.py
+++ Athan.py
@@ -2,31 +2,32 @@ import requests
 import json
 from datetime import datetime
 
+def fetchTimes():
+	response = requests.get("http://api.aladhan.com/v1/timingsByCity?city=gothenburg&country=sweden&method=2")
 
-response = requests.get("http://api.aladhan.com/v1/timingsByCity?city=gothenburg&country=sweden&method=2")
+	jsonData = response.json()['data']['timings']
+	fajr = jsonData['Fajr']
+	duhar = jsonData['Dhuhr']
+	asar = jsonData['Asr']
+	maghrib = jsonData['Maghrib']
+	isha = jsonData['Isha']
+	dailyTimings.append(fajr)
+	dailyTimings.append(duhar)
+	dailyTimings.append(asar)
+	dailyTimings.append(maghrib)
+	dailyTimings.append(isha)
+	print(dailyTimings)
 
-jsonData = response.json()['data']['timings']
-fajr = jsonData['Fajr']
-duhar = jsonData['Dhuhr']
-asar = jsonData['Asr']
-maghrib = jsonData['Maghrib']
-isha = jsonData['Isha']
+dailyTimings = []
 
-dailyTimings = [fajr,duhar,asar,maghrib,isha]
+fetchTimes()
 
-# Print the status code of the response.
-#print(response.status_code)
-print(dailyTimings)
-
-for time in dailyTimings:
-	print(time)
-
-
-
-while true: 
+while (1<2): 
 	now = datetime.now() # current date and time
 	current_time = now.strftime("%H:%M")
 
+	if ('00:01' == current_time):
+		fetchTimes()
 	if (current_time in dailyTimings): 
-		print("turn on the TV and play azan")  
+		print("turn on the TV and play azan")
 
