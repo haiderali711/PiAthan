4798ac1bd83125bf204cd4af350ec53a4e34dbe5
haiali <haiali@student.chalmers.se>
2020-06-04 18:26:09 +0200

    Update Athan.py
    Extraction of the Prayer times fromt he Json Response

--- Athan.py
+++ Athan.py
@@ -4,8 +4,18 @@ import json
 response = requests.get("http://api.aladhan.com/v1/timingsByCity?city=gothenburg&country=sweden&method=2")
 
 
-jsonData = response.json()
+jsonData = response.json()['data']['timings']
+fajr = jsonData['Fajr']
+duhar = jsonData['Dhuhr']
+asar = jsonData['Asr']
+maghrib = jsonData['Maghrib']
+isha = jsonData['Isha']
 
 # Print the status code of the response.
 #print(response.status_code)
 print(jsonData)
+print(fajr)
+print(duhar)
+print(asar)
+print(maghrib)
+print(isha)
